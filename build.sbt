f0ee5d0b389b789e9c36ad53aa723e47d450bf13
Spiro Michaylov <spiro@michaylov..com>
2015-04-18 08:44:03 -0700

    Prepare for 0.5.2

--- build.sbt
+++ build.sbt
@@ -8,6 +8,6 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "1.3.0" % "provided"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.3.0" % "provided"
 
-libraryDependencies += "com.github.spirom" %% "spark-mongodb-connector" % "0.5.1"
+libraryDependencies += "com.github.spirom" %% "spark-mongodb-connector" % "0.5.2"
 
 
--- src/main/scala/BasicQuery.scala
+++ src/main/scala/BasicQuery.scala
@@ -11,11 +11,11 @@ object BasicQuery {
     val coreConf =
       new SparkConf()
         .setAppName("BasicQuery")
-        .set("nsmc.connection.host", DBConfig.host)
-        .set("nsmc.connection.port", DBConfig.port)
+        .set("spark.nsmc.connection.host", DBConfig.host)
+        .set("spark.nsmc.connection.port", DBConfig.port)
     // if a username AND password are defined, use them
     val conf = (DBConfig.userName, DBConfig.password) match {
-      case (Some(u), Some(pw)) => coreConf.set("nsmc.user", u).set("nsmc.password", pw)
+      case (Some(u), Some(pw)) => coreConf.set("spark.nsmc.user", u).set("spark.nsmc.password", pw)
       case _ => coreConf
     }
     val sc = new SparkContext(conf)
